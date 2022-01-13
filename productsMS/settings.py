901504b58b8ae39ec9d33bfb90a58b0f1f58e863
Jan Wooden Mara <jan.mara.94@gmail.com>
2022-01-13 15:45:48 +0100

    static remove

--- productsMS/settings.py
+++ productsMS/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'catalog',
     'rest_framework'
 ]
@@ -112,13 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-DISABLE_COLLECTSTATIC=1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
